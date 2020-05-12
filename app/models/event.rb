e1c17e2409ef1e087bfdff47972ab406fe3969a2
TonyStark992 <maxime.castillo@gmail.com>
2020-05-12 19:34:59 +0200

    Ajout précisions dans validations Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
-  validates :start_date # ajouter : "ne doit pas être dans le passé"
-  validates :duration # ajouter : "multiple de 5 et positif"
-  validates :title, length: { in: 5..140 }
-  validates :description, length: { in: 20..1000 }
-  validates :price, length: { in: 1..1000 }
-  validates :location
+  validates :start_date, presence: true # ajouter : "ne doit pas être dans le passé"
+  validates :duration, presence: true # ajouter : "multiple de 5 et positif"
+  validates :title, presence: true, length: { in: 5..140 }
+  validates :description, presence: true, length: { in: 20..1000 }
+  validates :price, presence: true, length: { in: 1..1000 }
+  validates :location, presence: true
 
   has_many :attendances
   has_many :users, through: :attendances
